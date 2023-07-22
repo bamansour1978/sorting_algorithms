cf898ec89244d91e70acb570edf4422d3e9da250
bamansour1978 <karatman@gmail.com>
2023-07-22 18:16:19 +0100

    Fix sort insersion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,60 +1,37 @@
 #include "sort.h"
 
 /**
- * swap_n - Swap two nodes in a linked list.
- *
- * @h: A pointer to the head of the linked list.
- * @no1: A pointer to the first .
- * @no2: The second node to swap.
- *
- * Return: void
+ * insertion_sort_list - sorts a list of integers in ascending order
+ * using the Insertion sort algorithm
+ * @list: pointer to the doubly linked list
  */
-void swap_n(listint_t **h, listint_t **n1, listint_t *n2)
-{
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
-	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
-}
-/**
- * insertion_sort_list - insertion sort for a linked list
- *
- * @list: node's head for a linked list
- *
- */
-
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current_n, *swap_n, *prv_n;
+	listint_t *aux, *tmp, *prev;
 
-	if (!list || !*list)
+	if (!list)
 		return;
 
-	current_n = *list;
-	while ((current_n = current_n->next))
+	aux = *list;
+	tmp = aux->next;
+	while (tmp)
 	{
-		swap_n = current_n;
-		while (swap_n->prev && swap_n->n < swap_n->prev->n)
+		aux = tmp;
+		tmp = tmp->next;
+		prev = aux->prev;
+		while (prev && (aux->n < prev->n))
 		{
-			prv_n = swap_n->prev;
-			if (swap_n->next)
-				swap_n->next->prev = prv_n;
-			if (prv_n->prev)
-				prv_n->prev->next = swap_n;
-			else
-				*list = swap_n;
-			prv_n->next = swap_n->next;
-			swap_n->prev = prv_n->prev;
-			swap_n->next = prv_n;
-			prv_n->prev = swap_n;
-
+			if (prev->prev)
+				prev->prev->next = aux;
+			aux->prev = prev->prev;
+			prev->next = aux->next;
+			if (aux->next)
+				aux->next->prev = prev;
+			aux->next = prev;
+			prev->prev = aux;
+			if (!aux->prev)
+				*list = aux;
+			prev = aux->prev;
 			print_list(*list);
 		}
 	}
