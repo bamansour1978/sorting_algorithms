aed13b7f84f8b64bb65a542fffd634fa78b518c0
bamansour1978 <karatman@gmail.com>
2023-07-24 02:05:45 +0100

    FIX loop while error

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -58,6 +58,11 @@ void shell_sort(int *array, size_t size)
 
 	while (gap < size / 3)
 	{
+		gap *= 3;
+		gap += 1;
+	}
+	while (gap >= 1)
+	{
 		gap_sheel_sort(array, size, gap);
 		gap = (gap - 1) / 3;
 		print_array(array, size);
